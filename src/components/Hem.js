a23ba9fa9e0ddc9e6d9c9a01e60df769fb230ba0
daliu <daliu@DESKTOP-6DOHOH5>
2017-10-20 09:41:52 +0200

    boat reducer och index reducer

--- src/components/Hem.js
+++ src/components/Hem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 const Hem = () => (
  <div>
      Hem blir here blir here
-    
+   
  </div>
 )
 
--- src/reducers/boat.js
+++ src/reducers/boat.js
@@ -6,8 +6,31 @@ export default function() {
             Längd: 5.40 ,
             weight: 3.5,
             desc: "Pegazus 550 är en modern motorbåt med ergonomiskt utformad hytt. Det innovativt utformade skrovet och bredden tillsammans med högt fribord ger exceptionell stabilitet och användarsäkerhet. Om båten utrustas med en stark motor så möjliggörs snabb planing och hög hastighet. Råkar man ut för ett plötslig väderomslag ombord på Pegazus 550 behöver det inte innebära ett slut på den roliga resan, fäll bara upp kapellet som enkelt viks upp på en ram av rostfritt stål och ni sitter nu skyddade från regn och rusk. När ovädret har lagt sig kan man sedan vika ner och helt dölja kapellet i det bakre förvaringsutrymmet.",
-            img: "../img/550.jpg"
+            img: "../img/550.jpg",
+            price: 150000,
+            motor: "Yamaha 100standart"
+        },
+        {
+            id:2,
+            name:"Pegazus 560",
+            Längd: 7.40 ,
+            weight: 4.5,
+            desc: "Pegazus 560 560 560 560 560 560 560 560 skrovet och bredden tillsammans med högt fribord ger exceptionell stabilitet och användarsäkerhet. Om båten utrustas med en stark motor så möjliggörs snabb planing och hög hastighet. Råkar man ut för ett plötslig väderomslag ombord på Pegazus 550 behöver det inte innebära ett slut på den roliga resan, fäll bara upp kapellet som enkelt viks upp på en ram av rostfritt stål och ni sitter nu skyddade från regn och rusk. När ovädret har lagt sig kan man sedan vika ner och helt dölja kapellet i det bakre förvaringsutrymmet.",
+            img: "../img/550.jpg",
+            price: 170000,
+            motor: "Yamaha 100standart"
+        },
+        {
+            id:3,
+            name:"Pegazus 570",
+            Längd: 7.40 ,
+            weight: 4.5,
+            desc: "Pegazus 570 570 570  570 570 570 .Det innovativt utformade skrovet och bredden tillsammans med högt fribord ger exceptionell stabilitet och användarsäkerhet. Om båten utrustas med en stark motor så möjliggörs snabb planing och hög hastighet. Råkar man ut för ett plötslig väderomslag ombord på Pegazus 550 behöver det inte innebära ett slut på den roliga resan, fäll bara upp kapellet som enkelt viks upp på en ram av rostfritt stål och ni sitter nu skyddade från regn och rusk. När ovädret har lagt sig kan man sedan vika ner och helt dölja kapellet i det bakre förvaringsutrymmet.",
+            img: "../img/550.jpg",
+            price: 170000,
+            motor: "Yamaha 100standart"
         }
+
         
      
     ]
